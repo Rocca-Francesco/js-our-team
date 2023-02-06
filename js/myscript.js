a380372045b297eeb6b61cadfeb7d6f6fb918677
Rocca-Francesco <roccafrance98@yahoo.it>
2023-02-06 15:18:14 +0100

    array with objects

---
+++ js/myscript.js
@@ -0,0 +1,34 @@
+const ourTeam = [
+    {
+        foto: "../img/wayne-barnett-founder-ceo.jpg",
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO"
+    },
+    {
+        foto: "../img/angela-caroll-chief-editor.jpg",
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor"
+    },
+    {
+        foto: "../img/walter-gordon-office-manager.jpg",
+        nome: "Walter Gordon",
+        ruolo: "Office Manager"
+    },
+    {
+        foto: "../img/angela-lopez-social-media-manager.jpg",
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager"
+    },
+    {
+        foto: "../img/scott-estrada-developer.jpg",
+        nome: "Scott Estrada",
+        ruolo: "Developer"
+    },
+    {
+        foto: "../img/barbara-ramos-graphic-designer.jpg",
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer"
+    }
+];
+
+console.log(ourTeam);
